960cc7773b9eb7930a135ee68d10c804cf23a84a
Christiano <christianodvids@gmail.com>
2025-03-16 10:39:57 -0300

    feat: implement Triangle model with area and perimeter calcs

---
+++ src/main/java/com/geometria/triangulo/model/Triangle.java
@@ -0,0 +1,68 @@
+package com.geometria.triangulo.model;
+
+public class Triangle {
+    private double sideA;
+    private double sideB;
+    private double sideC;
+    private double base;
+    private double height;
+
+    public Triangle(double sideA, double sideB, double sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    public double calculateArea() {
+        return (base * height) / 2;
+    }
+
+    public double calculatePerimeter() {
+        return sideA + sideB + sideC;
+    }
+
+    public double getSideA() {
+        return sideA;
+    }
+
+    public void setSideA(double sideA) {
+        this.sideA = sideA;
+    }
+
+    public double getSideB() {
+        return sideB;
+    }
+
+    public void setSideB(double sideB) {
+        this.sideB = sideB;
+    }
+
+    public double getSideC() {
+        return sideC;
+    }
+
+    public void setSideC(double sideC) {
+        this.sideC = sideC;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
